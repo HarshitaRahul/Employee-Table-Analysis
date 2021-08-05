f8c9d142c01770e329d20a7472b78bb6cf4a567b
Harshita Rahul <harshi@harshitas-mbp.lan>
2021-08-05 13:49:10 -0400

    Adding more queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -100,7 +100,7 @@ LEFT JOIN employees AS e
 ON m.emp_no = e.emp_no;
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-SELECT e.emp_no,e.last_name,e.first_name,de.dept_no,d.dept_name
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
 FROM employees AS e
 INNER JOIN dept_emp AS de
 ON e.emp_no = de.emp_no
@@ -111,5 +111,27 @@ ON de.dept_no = d.dept_no;
 SELECT  first_name, last_name, sex
 FROM employees 
 WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
 
 
